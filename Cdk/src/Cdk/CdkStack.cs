f8a830da603b8e556289a83fb43e9ed5d96acf64
Benjamín <benjamin.diaze@outlook.com>
2025-06-13 00:49:00 -0400

    Se añade creación de rol para ser asumido por instancia web server

--- Cdk/src/Cdk/CdkStack.cs
+++ Cdk/src/Cdk/CdkStack.cs
@@ -1,7 +1,10 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.IAM;
 using Amazon.CDK.AWS.Route53;
 using Constructs;
 using System;
+using System.Collections.Generic;
+using System.Security.Principal;
 
 namespace Cdk
 {
@@ -13,6 +16,8 @@ namespace Cdk
             string domainName = System.Environment.GetEnvironmentVariable("DOMAIN_NAME") ?? throw new ArgumentNullException("DOMAIN_NAME");
             string subdomainName = System.Environment.GetEnvironmentVariable("SUBDOMAIN_NAME") ?? throw new ArgumentNullException("SUBDOMAIN_NAME");
             string ec2Host = System.Environment.GetEnvironmentVariable("EC2_HOST") ?? throw new ArgumentNullException("EC2_HOST");
+            string prefixRolesWebServer = System.Environment.GetEnvironmentVariable("PREFIX_ROLES_WEB_SERVER") ?? throw new ArgumentNullException("PREFIX_ROLES_WEB_SERVER");
+            string s3bucketArn = System.Environment.GetEnvironmentVariable("S3_BUCKET_ARN") ?? throw new ArgumentNullException("S3_BUCKET_ARN");
 
             IHostedZone hostedZone = HostedZone.FromLookup(this, $"{appName}WebServerHostedZone", new HostedZoneProviderProps {
                 DomainName = domainName
@@ -24,6 +29,31 @@ namespace Cdk
                 Zone = hostedZone,
                 RecordName = subdomainName,
                 DomainName = ec2Host
+            });
+
+            // Se crea rol de la subapp que asumirá la instancia web server...
+            _ = new Role(this, $"{appName}WebServerRole", new RoleProps {
+                RoleName = $"{prefixRolesWebServer}{appName}",
+                Description = $"Rol de {appName} para ser asumido por Web Server",
+                AssumedBy = new ServicePrincipal("ec2.amazonaws.com"),
+                InlinePolicies = new Dictionary<string, PolicyDocument> {
+                    {
+                        $"{appName}WebServerPolicy",
+                        new PolicyDocument(new PolicyDocumentProps{
+                            Statements = [
+                                new PolicyStatement(new PolicyStatementProps{
+                                    Sid = $"{appName}AccessToGetObject",
+                                    Actions = [
+                                        "s3:GetObject"
+                                    ],
+                                    Resources = [
+                                        $"{s3bucketArn}/*",
+                                    ],
+                                }),
+                            ]
+                        })
+                    }
+                }
             });
         }
     }
