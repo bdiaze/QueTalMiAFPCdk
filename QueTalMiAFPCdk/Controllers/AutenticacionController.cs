dc59f9c0ff1fe9c55bc8f36fb57a3830654f23f9
Benjamín <benjamin.diaze@outlook.com>
2025-08-16 21:04:49 -0400

    Se integra Cognito para iniciar sesión

---
+++ QueTalMiAFPCdk/Controllers/AutenticacionController.cs
@@ -0,0 +1,50 @@
+﻿using Amazon.Runtime.Internal.Transform;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QueTalMiAFPCdk.Models.Others;
+using QueTalMiAFPCdk.Services;
+using System.Security.Claims;
+
+namespace QueTalMiAFPCdk.Controllers {
+    public class AutenticacionController(ParameterStoreHelper parameterStoreHelper, IWebHostEnvironment environment) : Controller {
+        [Authorize]
+        [Route("login")]
+        public IActionResult Login() {
+            string? nameIdentifier = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string? email = User.FindFirstValue(ClaimTypes.Email);
+            string? givenName = User.FindFirstValue(ClaimTypes.GivenName);
+            string? surname = User.FindFirstValue(ClaimTypes.Surname);
+
+            return RedirectToAction("Index", "Resumen");
+        }
+
+        [Route("logout")]
+        public async Task<IActionResult> Logout() {
+            if (User.Identity != null && User.Identity.IsAuthenticated) {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+
+                string cognitoBaseUrl = await parameterStoreHelper.ObtenerParametro("/QueTalMiAFP/Cognito/BaseUrl");
+                string userPoolClientId = await parameterStoreHelper.ObtenerParametro("/QueTalMiAFP/Cognito/UserPoolClientId");
+                string[] cognitoLogouts = (await parameterStoreHelper.ObtenerParametro("/QueTalMiAFP/Cognito/Logouts")).Split(",");
+
+                string logoutUrl = cognitoLogouts.First(l => !l.Contains("localhost"));
+                if (environment.IsDevelopment()) {
+                    logoutUrl = cognitoLogouts.First(l => l.Contains("localhost"));
+                }
+
+                return Redirect($"{cognitoBaseUrl}/logout?client_id={userPoolClientId}&logout_uri={logoutUrl}");
+            }
+
+            return RedirectToAction("Index", "Resumen");
+        }
+
+        [Route("PoliticaPrivacidad")]
+        public IActionResult PoliticaPrivacidad() {
+            return View();
+        }
+    }
+}
