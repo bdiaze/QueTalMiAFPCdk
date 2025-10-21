a607263521d4ca348a5da0c9f3a8c7165be10f99
Benjamín <benjamin.diaze@outlook.com>
2025-10-20 22:49:55 -0300

    Se añade log y se cambia manejo de error de pantalla de error

--- QueTalMiAFPCdk/Controllers/ErrorController.cs
+++ QueTalMiAFPCdk/Controllers/ErrorController.cs
@@ -1,15 +1,29 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace QueTalMiAFPCdk.Controllers {
-	public class ErrorController : Controller {
+	public class ErrorController(ILogger<ErrorController> logger) : Controller {
         public IActionResult Index() {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
 			IExceptionHandlerFeature? exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
-			ViewBag.ClaseError = exception!.Error.GetType().FullName;
-			ViewBag.StatusCode = HttpContext.Response.StatusCode;
-			ViewBag.Message = exception.Error.Message;
-			ViewBag.StackTrace = exception.Error.StackTrace;
-			return View();
+
+            if (exception != null) {
+                ViewData["ClaseError"] = exception.Error.GetType().FullName;
+                if (ViewData["ClaseError"]?.ToString() == "QueTalMiAFP.Models.Others.ExceptionQueTalMiAFP") {
+                    ViewData["Message"] = exception.Error.Message;
+                }
+            }
+
+            logger.LogInformation(
+                "[GET] - [Error] - [Index] - [{ElapsedTime} ms] - [{StatusCode}] - " +
+                "Ocurrió un error no controlado en la aplicación. Autenticado: {IsAutheticated} - " +
+                "Path: {Path} - Error: {Error}.",
+                stopwatch.ElapsedMilliseconds, StatusCodes.Status200OK, User.Identity?.IsAuthenticated ?? false,
+                exception?.Path, exception?.Error.ToString().Replace("\r", "").Replace("\n", "\\n"));
+
+            return View();
 		}
 	}
 }
