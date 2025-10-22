008e7daac6bebca8639ec6fc227e7182e20e0ca0
Benjamín <benjamin.diaze@outlook.com>
2025-10-22 20:57:30 -0300

    Se añaden logs en pantalla de donación.

--- QueTalMiAFPCdk/Controllers/DonacionController.cs
+++ QueTalMiAFPCdk/Controllers/DonacionController.cs
@@ -3,20 +3,32 @@ using MercadoPago.Resource.Preference;
 using Microsoft.AspNetCore.Mvc;
 using QueTalMiAFPCdk.Models.ViewModels;
 using QueTalMiAFPCdk.Services;
+using System.Diagnostics;
 
 namespace QueTalMiAFPCdk.Controllers {
-    public class DonacionController(MercadoPagoHelper mercadoPagoHelper) : Controller {
+    public class DonacionController(ILogger<DonacionController> logger, MercadoPagoHelper mercadoPagoHelper) : Controller {
         public IActionResult Index() {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             DonacionViewModel model = new() { 
                 Monto = "$1.000"
             };
             ViewBag.MontoCargado = false;
             ViewBag.OcultarCompleto = true;
+
+            logger.LogInformation(
+                "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
+                "Se retorna exitosamente la página de donación.",
+                HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
+                stopwatch.ElapsedMilliseconds, StatusCodes.Status200OK, User.Identity?.IsAuthenticated ?? false);
+
             return View(model);
         }
 
         [HttpPost]
         public async Task<IActionResult> EmitiendoPago(DonacionViewModel model) {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             // Se le quita el formato al monto ingresado...
             string montoSinFormato = "";
             for (int i = 0; i < model.Monto.Length; i++) {
@@ -52,13 +64,28 @@ namespace QueTalMiAFPCdk.Controllers {
             ViewBag.MontoCargado = true;
             ViewBag.OcultarCompleto = true;
 
+            logger.LogInformation(
+                "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
+                "Se retorna exitosamente la página de emisión de pago.",
+                HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
+                stopwatch.ElapsedMilliseconds, StatusCodes.Status200OK, User.Identity?.IsAuthenticated ?? false);
+
             return View("Index", model);    
         }
 
         [Route("[controller]/[action]/{resultado}")]
         public IActionResult RetornoMercadoPago(string resultado) {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             ViewBag.Resultado = resultado.ToLower();
             ViewBag.OcultarCompleto = true;
+
+            logger.LogInformation(
+                "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
+                "Se retorna exitosamente la página de retorno de mercado pago.",
+                HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
+                stopwatch.ElapsedMilliseconds, StatusCodes.Status200OK, User.Identity?.IsAuthenticated ?? false);
+
             return View();
         }
     }
