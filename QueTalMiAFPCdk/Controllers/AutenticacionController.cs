11d394f99840ad28fc6b70758ddcecdbadaeb8ff
Benjamín <benjamin.diaze@outlook.com>
2025-10-22 22:22:22 -0300

    Se corrige log de política de privacidad

--- QueTalMiAFPCdk/Controllers/AutenticacionController.cs
+++ QueTalMiAFPCdk/Controllers/AutenticacionController.cs
@@ -113,7 +113,7 @@ namespace QueTalMiAFPCdk.Controllers {
                 "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
                 "Se retorna exitosamente la página de política de privacidad.",
                 HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
-                stopwatch.ElapsedMilliseconds, StatusCodes.Status302Found, User.Identity?.IsAuthenticated ?? false);
+                stopwatch.ElapsedMilliseconds, StatusCodes.Status200OK, User.Identity?.IsAuthenticated ?? false);
 
             return View();
         }
