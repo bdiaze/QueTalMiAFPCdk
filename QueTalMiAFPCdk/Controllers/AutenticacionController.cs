7d59770f57412f57000a852e35a4f607daac526e
Benjamín <benjamin.diaze@outlook.com>
2025-10-22 21:50:02 -0300

    Se añaden logs en página de autenticación.

--- QueTalMiAFPCdk/Controllers/AutenticacionController.cs
+++ QueTalMiAFPCdk/Controllers/AutenticacionController.cs
@@ -4,29 +4,52 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Utilities;
 using QueTalMiAFPCdk.Models.Others;
 using QueTalMiAFPCdk.Services;
+using System.Diagnostics;
 using System.Security.Claims;
 
 namespace QueTalMiAFPCdk.Controllers {
-    public class AutenticacionController(ParameterStoreHelper parameterStoreHelper, IWebHostEnvironment environment) : Controller {
+    public class AutenticacionController(ILogger<AutenticacionController> logger, ParameterStoreHelper parameterStoreHelper, IWebHostEnvironment environment) : Controller {
         [Authorize]
         [Route("login")]
         public IActionResult Login(string? redirect = null) {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             string? nameIdentifier = User.FindFirstValue(ClaimTypes.NameIdentifier);
             string? email = User.FindFirstValue(ClaimTypes.Email);
             string? givenName = User.FindFirstValue(ClaimTypes.GivenName);
             string? surname = User.FindFirstValue(ClaimTypes.Surname);
 
             if (!string.IsNullOrEmpty(redirect)) {
+
+                logger.LogInformation(
+                    "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
+                    "Se redirecciona a redirect indicado - " +
+                    "Redirect: {Redirect}.",
+                    HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
+                    stopwatch.ElapsedMilliseconds, StatusCodes.Status302Found, User.Identity?.IsAuthenticated ?? false,
+                    redirect);
+
                 return Redirect(redirect);
             }
+
+            logger.LogInformation(
+                "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
+                "Se redirecciona a página de notificaciones - " +
+                "Redirect: {Redirect}.",
+                HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
+                stopwatch.ElapsedMilliseconds, StatusCodes.Status302Found, User.Identity?.IsAuthenticated ?? false,
+                redirect);
 
             return RedirectToAction("Index", "Notificaciones");
         }
 
         [Route("logout")]
         public async Task<IActionResult> Logout(string? redirect = null) {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             if (User.Identity != null && User.Identity.IsAuthenticated) {
                 await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
                 await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
@@ -44,21 +67,54 @@ namespace QueTalMiAFPCdk.Controllers {
                     TempData["PostLogoutRedirect"] = redirect;
                 }
 
+                logger.LogInformation(
+                    "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
+                    "Se redirecciona a login de cognito almacenando redirect - " +
+                    "Redirect: {Redirect}.",
+                    HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
+                    stopwatch.ElapsedMilliseconds, StatusCodes.Status302Found, User.Identity?.IsAuthenticated ?? false,
+                    redirect);
+
                 return Redirect($"{cognitoBaseUrl}/logout?client_id={userPoolClientId}&logout_uri={logoutUrl}");
             }
 
             if (TempData.TryGetValue("PostLogoutRedirect", out object? value)) {
                 string? storedRedirect = value?.ToString();
                 if (!string.IsNullOrEmpty(storedRedirect)) {
+
+                    logger.LogInformation(
+                        "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
+                        "Se redirecciona a stored redirect - " +
+                        "Stored Redirect: {StoredRedirect} - Redirect: {Redirect}.",
+                        HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
+                        stopwatch.ElapsedMilliseconds, StatusCodes.Status302Found, User.Identity?.IsAuthenticated ?? false,
+                        storedRedirect, redirect);
+
                     return Redirect(storedRedirect);
                 }
             }
+
+            logger.LogInformation(
+                "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
+                "Se redirecciona a página de resumen - " +
+                "Redirect: {Redirect}.",
+                HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
+                stopwatch.ElapsedMilliseconds, StatusCodes.Status302Found, User.Identity?.IsAuthenticated ?? false,
+                redirect);
 
             return RedirectToAction("Index", "Resumen");
         }
 
         [Route("PoliticaPrivacidad")]
         public IActionResult PoliticaPrivacidad() {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            logger.LogInformation(
+                "[{Method}] - [{Controller}] - [{Action}] - [{ElapsedTime} ms] - [{StatusCode}] - [Usuario Autenticado: {IsAuthenticated}] - " +
+                "Se retorna exitosamente la página de política de privacidad.",
+                HttpContext.Request.Method, ControllerContext.ActionDescriptor.ControllerName, ControllerContext.ActionDescriptor.ActionName,
+                stopwatch.ElapsedMilliseconds, StatusCodes.Status302Found, User.Identity?.IsAuthenticated ?? false);
+
             return View();
         }
     }
